fb35f22f6edd988ec3da7cd2303a771f8e89d514
Miguel <miguel@terranigma-solutions.com>
2023-06-01 10:32:15 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+﻿from setuptools import setup
+
+setup(
+    name='LiquidEarthAPI',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='EUGPL',
+    author='MigueldelaVarga',
+    author_email='miguel@terranigma-solutions.com',
+    description=''
+)
