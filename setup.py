dd01f334c21f5b8c3577455dd3102bac252afeb1
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-15 13:32:43 +0200

    - [ENH/BUG] Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 ﻿from setuptools import setup, find_packages
 
+def read_requirements(file_name):
+    with open(file_name, "r", encoding="utf-8") as f:
+        return [line.strip() for line in f.readlines()]
+
 setup(
     name='LiquidEarthAPI',
     version='0.1',
@@ -8,5 +12,6 @@ setup(
     license='EUGPL',
     author='MigueldelaVarga',
     author_email='miguel@terranigma-solutions.com',
-    description=''
+    description='',
+    install_requires=read_requirements("requirements.txt"),
 )
