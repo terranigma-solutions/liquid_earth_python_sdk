bccb1ef09aa90f90caea1c7565b4c9214a6a9574
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-04-25 11:36:13 +0200

    [ENH] Improve setup.py and requirements handling
    Updated setup.py for better package information and handling. Refined the read_requirements function to manage the requirements more efficiently. Also, incorporated setuptools_scm for automated management of package versioning.

--- setup.py
+++ setup.py
@@ -1,19 +1,40 @@
-﻿from setuptools import setup, find_packages
+﻿from os import path
+
+from setuptools import setup, find_packages
 
 
 def read_requirements(file_name):
+    requirements = []
     with open(file_name, "r", encoding="utf-8") as f:
-        return [line.strip() for line in f.readlines()]
+        for line in f:
+            # Strip whitespace and ignore comments
+            line = line.strip()
+            if line.startswith("#") or not line:
+                continue
+
+            # Handle -r directive
+            if line.startswith("-r "):
+                referenced_file = line.split()[1]  # Extract the file name
+                requirements.extend(read_requirements(referenced_file))  # Recursively read referenced file
+            else:
+                requirements.append(line)
+    return requirements
 
 
 setup(
-    name='LiquidEarthAPI',
-    version='0.1',
-    packages=find_packages(),
+    name='liquid_earth_api',
+    packages=find_packages(exclude=('test', 'docs', 'examples')),
     url='',
-    license='EUGPL',
-    author='MigueldelaVarga',
+    license='EUPL-v1.2',
+    author='Miguel de la Varga',
     author_email='miguel@terranigma-solutions.com',
-    description='',
+    description='Python API to interact with Liquid Earth',
     install_requires=read_requirements("requirements.txt"),
+    setup_requires=['setuptools_scm'],
+    use_scm_version={
+            "root"            : ".",
+            "relative_to"     : __file__,
+            "write_to"        : path.join("liquid_earth_api", "_version.py"),
+            "fallback_version": "0.0.1"
+    },
 )
