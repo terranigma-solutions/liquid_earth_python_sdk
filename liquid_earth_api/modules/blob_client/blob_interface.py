ca468ae706e74bf3abee2126173d99391e90f309
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-10-31 14:01:37 +0100

    [ENH/TEST] Adding example of using the python api to upload dxf to reveal

--- liquid_earth_api/modules/blob_client/blob_interface.py
+++ liquid_earth_api/modules/blob_client/blob_interface.py
@@ -8,13 +8,11 @@ import subsurface
 def push_unstructured_data(unstructured_data: subsurface.UnstructuredData, sas_dict: dict):
     meshes = unstructured_data
     try:
-        data, header = meshes.to_binary()
+        data = meshes.to_binary()
         blob_client = BlobClient.from_blob_url(sas_dict["sasForTestLe"])
         _ = blob_client.upload_blob(data, overwrite=True)
-        blob_client = BlobClient.from_blob_url(sas_dict["sasForTestJson"])
-        _ = blob_client.upload_blob(json.dumps(header), overwrite=True)
         return True
     except Exception as e:
         print(f"Error uploading files: {e}")
-        ValueError("Error uploading files")
+        raise ValueError("Error uploading files")
         
--- tests/test_subsurface_link.py
+++ tests/test_subsurface_link.py
@@ -1,9 +1,8 @@
 ﻿import os
 
-from dotenv import dotenv_values, load_dotenv
+from dotenv import load_dotenv
 
-from liquid_earth_api.data.schemas import AddDataPostData, AddNewSpacePostData
-from liquid_earth_api.api import le_api, utils_api
+from liquid_earth_api.api import le_api
 
 load_dotenv()
 
@@ -12,15 +11,31 @@ user_token = os.environ.get("LIQUID_EARTH_API_TOKEN")
 space_name = "Ideon demo space"
 
 def test_dxf_mesh_to_existing_space():
-    # Read and digest the DXF file
+    import subsurface as ss
+    from subsurface.modules.reader import dxf_file_to_unstruct_input
+    import pandas 
     
-    
-    return 
-    foo = le_api.upload_mesh_to_existing_space(
-        space_name="Ideon demo space",
-        data=None,
-        file_name="dxf_file",
+    path = os.environ.get("DXF_DATA_PATH")
+    vertex, cells, cell_attr_int, cell_attr_map = dxf_file_to_unstruct_input(path)
+
+    unstruct = ss.UnstructuredData.from_array(
+        vertex,
+        cells,
+        cells_attr=pandas.DataFrame(cell_attr_int, columns=["Id"]),
+        xarray_attributes={"cell_attr_map": cell_attr_map},
+    )
+
+    ts = ss.TriSurf(mesh=unstruct)
+    s = ss.visualization.to_pyvista_mesh(ts)
+    if False:
+        ss.visualization.pv_plot([s], image_2d=False)
+
+    link = le_api.upload_mesh_to_existing_space(
+        space_name="Ideon Demo (exported from geoh5)",
+        data=unstruct,
+        file_name="upc_surface_api",
         token=user_token
     )
+    #'https://reveal-earth.app.link/1067130d-29ec-4daa-b6e4-b23d833fa625'
     pass
 
