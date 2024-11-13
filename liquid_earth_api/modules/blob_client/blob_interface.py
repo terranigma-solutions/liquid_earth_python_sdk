50a7762da4d1734ab208e10ab2dbbd64e2f8808d
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-11-13 14:24:45 +0100

    [WIP/TEST] Making some examples using python sdk

--- liquid_earth_api/modules/blob_client/blob_interface.py
+++ liquid_earth_api/modules/blob_client/blob_interface.py
@@ -1,4 +1,4 @@
-import json
+from io import BytesIO
 
 from azure.storage.blob import BlobClient
 
@@ -10,9 +10,17 @@ def push_unstructured_data(unstructured_data: subsurface.UnstructuredData, sas_d
     try:
         data = meshes.to_binary()
         blob_client = BlobClient.from_blob_url(sas_dict["sasForTestLe"])
-        _ = blob_client.upload_blob(data, overwrite=True)
+        stream = BytesIO(data)
+        val = blob_client.upload_blob(
+            stream,
+            blob_type="BlockBlob",
+            overwrite=True,
+            timeout=30
+        )
+        print(f"Uploaded file: {val}")
         return True
     except Exception as e:
         print(f"Error uploading files: {e}")
-        raise ValueError("Error uploading files")
-        
+        raise ValueError(f"Error uploading files: {e}")
+
+
--- tests/test_subsurface_link.py
+++ tests/test_subsurface_link.py
@@ -10,12 +10,13 @@ user_token = os.environ.get("LIQUID_EARTH_API_TOKEN")
 
 space_name = "Ideon demo space"
 
+
 def test_dxf_mesh_to_existing_space():
     import subsurface as ss
     from subsurface.modules.reader import dxf_file_to_unstruct_input
-    import pandas 
-    
-    path = os.environ.get("DXF_DATA_PATH")
+    import pandas
+
+    path = os.environ.get("PATH_TO_DXF_DATA_PATH_SMALL")
     vertex, cells, cell_attr_int, cell_attr_map = dxf_file_to_unstruct_input(path)
 
     unstruct = ss.UnstructuredData.from_array(
@@ -31,11 +32,32 @@ def test_dxf_mesh_to_existing_space():
         ss.visualization.pv_plot([s], image_2d=False)
 
     link = le_api.upload_mesh_to_existing_space(
-        space_name="Ideon Demo (exported from geoh5)",
+        # space_name="Demo (exported from geoh5)",
+        space_name="Shared Example: MX",
         data=unstruct,
-        file_name="upc_surface_api",
-        token=user_token
+        file_name="dxf",
+        token=user_token,
+        grab_link=False
     )
-    #'https://reveal-earth.app.link/1067130d-29ec-4daa-b6e4-b23d833fa625'
     pass
 
+
+def test_mx_mesh_to_existing_space():
+    import subsurface as ss
+    from subsurface.modules.reader.mesh.mx_reader import mx_to_unstruct_from_file
+    unstruct: ss.UnstructuredData = mx_to_unstruct_from_file(os.getenv("PATH_TO_MX"))
+
+    ts = ss.TriSurf(mesh=unstruct)
+    s = ss.visualization.to_pyvista_mesh(ts)
+    ss.visualization.pv_plot([s], image_2d=True)
+
+    new_file = open(f"mx.le", "wb")
+    new_file.write(unstruct.to_binary())
+
+    link = le_api.upload_mesh_to_existing_space(
+        space_name="Shared Example: MX",
+        data=unstruct,
+        file_name="mx_file.le",
+        token=user_token
+    )
+    pass
