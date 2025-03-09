ce97cd47f3efb2bcc9eb392c1c20846d86a31521
miguel <miguel@TeamCity>
2025-03-09 10:47:47 +0000

    TeamCity change in 'Liquid Earth Python Sdk' project: parameters of 'Testing Dev 001' build configuration were updated

--- .teamcity/patches/buildTypes/TestingDev001.kts
+++ .teamcity/patches/buildTypes/TestingDev001.kts
@@ -22,5 +22,11 @@ changeBuildType(RelativeId("TestingDev001")) {
         update {
             password("env.LIQUID_EARTH_API_TOKEN_LEGACY", "credentialsJSON:e740f821-25ac-410c-9f81-2b8cb7c7911e")
         }
+        expect {
+            param("env.LIQUID_EARTH_API_TOKEN_REVEAL", "credentialsJSON:ApiToken_Reveal")
+        }
+        update {
+            password("env.LIQUID_EARTH_API_TOKEN_REVEAL", "credentialsJSON:d14b7073-3cd7-4e10-a829-2a094b2576a8")
+        }
     }
 }
