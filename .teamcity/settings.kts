613744a07737b6cb2d9a96fce2057dbca8554b10
Miguel <miguel@terranigma-solutions.com>
2025-03-09 10:45:13 +0000

    [ENH] Update backend and API token configurations in settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -49,27 +49,34 @@ object TestingDev001 : BuildType({
     name = "Testing Dev 001"
 
     params {
-        param("env.PATH_TO_WEISWEILER", "%env.TERRA_PATH_DEVOPS%/meshes/Weisweiler/")
-        param("env.PATH_TO_OBJ_GALLERIES_I", "%env.TERRA_PATH_DEVOPS%/meshes/OBJ/Galleries/Solid1.obj")
-        param("env.PATH_TO_OBJ_SCANS", "%env.TERRA_PATH_DEVOPS%/meshes/OBJ/Core scans Boliden/rsrbF9l2zc/model.obj")
-        param("REQUIREMENT_LEVEL", "READ_MESH")
-        param("env.PATH_TO_OBJ_MULTIMATERIAL_II", "%env.TERRA_PATH_DEVOPS%/meshes/OBJ/MultiMaterialObj/MultiMaterialObj.obj")
-        param("env.PATH_TO_INTERPRETATION", "%env.TERRA_PATH_DEVOPS%/meshes/Seismic/Anl2-1.tif")
-        param("env.PATH_TO_OBJ", "%env.TERRA_PATH_DEVOPS%/meshes/OBJ/Broadhaven_Obj/model/model.obj")
-        param("env.PATH_TO_OBJ_FACE_II", "%env.TERRA_PATH_DEVOPS%/meshes/OBJ/Faces/Lapp_458_Rum_19_2_2024-02-21_0820_scan5_geoim4.obj")
-        param("env.TERRA_PATH_DEVOPS", """D:\OneDrive - Terranigma Solutions GmbH/Documents - Terranigma Base/DevOps/SubsurfaceTestData/""")
-        param("env.PATH_TO_OBJ_FACE_I", "%env.TERRA_PATH_DEVOPS%/meshes/OBJ/Faces/face1.obj")
-        param("env.PATH_TO_SEISMIC_FINAL", "%env.TERRA_PATH_DEVOPS%/meshes/Seismic/L1_final-mig.sgy")
-        param("env.PATH_TO_GLB", "%env.TERRA_PATH_DEVOPS%/meshes/GLB - GLTF/Duck.glb")
-        param("env.PATH_TO_SEISMIC", "%env.TERRA_PATH_DEVOPS%/meshes/Seismic/Linie01.segy")
-        param("env.PATH_TO_MAGNETIC_INTERPRETATION", "%env.TERRA_PATH_DEVOPS%/meshes/Magnetic/Plate 2a - Profile 1 2D inversion.pdf")
-        param("env.MPLBACKEND", "Agg")
-        param("env.PATH_TO_BOLIDEN", "%env.TERRA_PATH_DEVOPS%/combined/Leapfrog_OMF/Garpenberg_global_20220715.omf")
-        param("env.PATH_TO_ASCII_DRILLHOLES", "%env.TERRA_PATH_DEVOPS%/boreholes/ASCII_drillholes/")
-        param("env.PATH_TO_SECTION", "%env.TERRA_PATH_DEVOPS%/meshes/Seismic/L1_CDP-Coords.txt")
-        param("env.PATH_TO_GLB_COMPLEX", "%env.TERRA_PATH_DEVOPS%/meshes/GLB - GLTF/GlbFile.glb")
-        param("env.PATH_TO_MTL", "%env.TERRA_PATH_DEVOPS%/meshes/OBJ/Broadhaven_Obj/model/model.mtl")
-        param("env.PATH_TO_OMF", "%env.TERRA_PATH_DEVOPS%/combined/Leapfrog_OMF/Collinstown.omf")
+        // Backend configuration
+        param("env.BACKEND_OVERRIDE_DEV", "https://liquidearthapim-dev001.azure-api.net/python/")
+        param("env.BACKEND_OVERRIDE_LOCAL", "http://localhost:7151/api")
+        param("env.BACKEND_OVERRIDE", "%env.BACKEND_OVERRIDE_DEV%")
+        
+        // API Tokens (using placeholder values for security)
+        param("env.LIQUID_EARTH_API_TOKEN_REVEAL", "credentialsJSON:ApiToken_Reveal")
+        param("env.LIQUID_EARTH_API_TOKEN_LEGACY", "credentialsJSON:ApiToken_Legacy")
+        param("env.LIQUID_EARTH_API_TOKEN_DEV001", "credentialsJSON:ApiToken_Dev001")
+        param("env.LIQUID_EARTH_API_TOKEN", "%env.LIQUID_EARTH_API_TOKEN_DEV001%")
+        
+        // Login configuration
+        param("env.LOGIN_URL", "https://liquidearthapim-dev001.azure-api.net/user/login_b2c_license")
+        param("env.LOGIN_USER", "credentialsJSON:LoginUser")
+        param("env.LOGIN_PASSWORD", "credentialsJSON:LoginPassword")
+        param("env.LOGIN_CLIENT_ID", "685e08c0-0aac-42f6-80a9-c57440cd2962")
+        param("env.LOGIN_SCOPE", "%env.LOGIN_CLIENT_ID%")
+        param("env.LOGIN_SUBSCRIPTION_KEY", "credentialsJSON:SubscriptionKey")
+        
+        // Path configurations
+        param("env.DXF_DATA_PATH", "%env.TERRA_PATH_DEVOPS%/meshes/upc_surface.dxf")
+        param("env.DXF_DATA_PATH_II", "%env.TERRA_PATH_DEVOPS%/meshes/leinster_detector_positions.dxf")
+        param("env.PATH_TO_MX", "%env.TERRA_PATH_DEVOPS%/volume/GOCAD/mix/horizons_faults.mx")
+        param("env.PATH_TO_DXF_DATA_PATH_SMALL", "%env.TERRA_PATH_DEVOPS%/meshes/shafts_small.dxf")
+        
+        // Test user configuration
+        param("env.TEST_USER_ID", "43773b72-feae-495d-a1d9-e529dc64da9f")
+        param("env.TEST_LOGIN_TOKEN", "credentialsJSON:TestLoginToken")
     }
 
     vcs {
