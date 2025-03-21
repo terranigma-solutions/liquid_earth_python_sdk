8891658fb3c6b7fb4f28cee2810c1ebb49fb1503
Miguel <miguel@terranigma-solutions.com>
2025-03-21 17:57:28 +0000

    [WIP/TEST] Reviving gempy tests

--- tests/test_le_api_gempy_test.py
+++ tests/test_le_api_gempy_test.py
@@ -1,42 +1,50 @@
-﻿import pytest
+﻿from subsurface import optional_requirements
 
 from liquid_earth_api.api import le_api
+from liquid_earth_api.data.schemas import DeleteSpacePostData
 from .test_le_api_test_cases import TestLEApiBase
 
 
-@pytest.mark.skip("Requires GemPy")
 class TestLEApiWithGempy(TestLEApiBase):
     """Tests that require GemPy"""
-
+    _space_id: str
+    
     @classmethod
     def setup_class(cls):
         super().setup_class()
         import gempy as gp
         from gempy.core.data.enumerators import ExampleModel
         cls.gempy_model = gp.generate_example_model(ExampleModel.ANTICLINE, compute_model=True)
-
+        
+     
+    @classmethod
+    def teardown_class(cls):
+        """Deletes the created space after the tests"""
+        super().teardown_class()
+        le_api.delete_space(
+            delete_space_post_data=DeleteSpacePostData(spaceId=_project.spaceId),
+            token=cls.user_token
+        )
+    
     def test_upload_mesh_to_existing_space(self):
+        ss = optional_requirements.require_subsurface()
+        subsurface: ss.UnstructuredData = self.gempy_model.solutions.raw_arrays.meshes_to_subsurface()
+        name = self.space_name
+        token = self.user_token
+        
         response = le_api.upload_mesh_to_existing_space(
-            space_name=self.space_name,
-            data=self.gempy_model.solutions.raw_arrays.meshes_to_subsurface(),
-            file_name="test2",
-            token=self.user_token
+            space_name="[TEMP] Test python api",
+            data=subsurface,
+            file_name="anticline",
+            token=token
         )
         assert response is not None
 
     def test_upload_mesh_to_new_space(self):
         response = le_api.upload_mesh_to_new_space(
-            space_name="new_space_name",
+            space_name="[TEMP] Test python api",
             data=self.gempy_model.solutions.raw_arrays.meshes_to_subsurface(),
-            file_name="test2",
-            token=self.user_token
-        )
-        assert response is not None
-
-    def test_upload_data_to_space(self):
-        response = le_api.post_add_data_to_space(
-            unstructured_data=self.gempy_model.solutions.raw_arrays.meshes_to_subsurface(),
-            post_data=self._get_test_project(self.space_name),
+            file_name="anticline",
             token=self.user_token
         )
         assert response is not None
