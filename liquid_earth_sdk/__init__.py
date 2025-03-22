6f33737453a35561f5327e8b5dfd4322e0dbf6a0
Miguel <miguel@terranigma-solutions.com>
2025-03-22 11:41:09 +0000

    [ENH] Add version retrieval for subsurface package with fallback handling

--- liquid_earth_sdk/__init__.py
+++ liquid_earth_sdk/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +9,17 @@ from liquid_earth_sdk.api.le_api import (
     get_available_projects,
     get_deep_link
 )
+# Version.
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # For Python <3.8, fallback
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("subsurface")  # Use package name
+except ImportError:
+    # If it was not installed, then we don't know the version. We could throw a
+    # warning here, but this case *should* be rare. subsurface should be
+    # installed properly!
+    __version__ = 'unknown-'+datetime.today().strftime('%Y%m%d')
