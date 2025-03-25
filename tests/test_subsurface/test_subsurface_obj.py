4e3475691f8f3840d84ed6e032ba30126f5c7556
Miguel <miguel@terranigma-solutions.com>
2025-03-25 11:27:05 +0000

    [WIP] Preparing the rest of tests

--- tests/test_subsurface/test_subsurface_obj.py
+++ tests/test_subsurface/test_subsurface_obj.py
@@ -1,11 +1,16 @@
 ﻿import os
 
+import pytest
+
 import subsurface as ss
+from liquid_earth_sdk.core.output import ServerResponse
 
 from subsurface.modules.reader.mesh._trimesh_reader import _load_with_trimesh
 from liquid_earth_sdk.api import le_api
 
+ONLY_EXPLICIT = True
 
+@pytest.mark.skipif(ONLY_EXPLICIT, reason="Run Explicit")
 def test_obj_one_element_no_texture():
     path_to_obj = os.getenv("TERRA_PATH_DEVOPS") + "/meshes/OBJ/TexturedMesh/PenguinBaseMesh.obj"
     trimesh_obj = _load_with_trimesh(
@@ -20,8 +25,8 @@ def test_obj_one_element_no_texture():
 
     if True:
         unstruct: ss.UnstructuredData = ts.mesh
-        link = le_api.upload_mesh_to_new_space(
-            space_name="[TEMP] DXF From SDK",
+        response: ServerResponse = le_api.upload_mesh_to_new_space(
+            space_name="[TEMP] Test PythonSDk: OBJ one element no texture",
             data=unstruct,
             file_name="obj_one_element_no_texture",
             token=(os.environ.get("LIQUID_EARTH_API_TOKEN")),
@@ -29,3 +34,18 @@ def test_obj_one_element_no_texture():
         )
         
         pass
+
+
+@pytest.mark.skipif(ONLY_EXPLICIT, reason="Run Explicit")
+def test_OBJ_trimesh_THREE_ELEMENTS_no_texture_to_unstruct():
+    pass
+
+
+@pytest.mark.skipif(ONLY_EXPLICIT, reason="Run Explicit")
+def test_OBJ_ONE_element_texture_to_unstruct():
+    pass
+
+
+@pytest.mark.skipif(ONLY_EXPLICIT, reason="Run Explicit")
+def test_OBJ_three_element_texture_to_unstruct():
+    pass
